3489040a576d6d55008c59efdda5c943a5d0afc6
Monica Paultre <paulmj1@mail.broward.edu>
2025-02-25 15:47:06 -0500

    moved and altered

--- src/script.js
+++ src/script.js
@@ -87,103 +87,116 @@ function applyStyle(styleNumber) {
 
         case 2: // Rainbow style
     
-        var image = document.createElement('img');  
-        image.setAttribute('id', 'image-bg');  
-        //image.setAttribute('src', './assets/style2.gif');  
-        image.style.position = 'fixed';  
-        image.style.top = '0';
-        image.style.left = '0';
-        image.style.width = '100vw';
-        image.style.height = '100vh';
-        image.style.objectFit = 'repeat';  
-        image.style.zIndex = '-1';
- 
-        document.body.prepend(image); 
+            var image = document.createElement('img');  
+            image.setAttribute('id', 'image-bg');  
+            //image.setAttribute('src', './assets/style2.gif');  
+            image.style.position = 'fixed';  
+            image.style.top = '0';
+            image.style.left = '0';
+            image.style.width = '100vw';
+            image.style.height = '100vh';
+            image.style.objectFit = 'repeat';  
+            image.style.zIndex = '-1';
+    
+            document.body.prepend(image); 
 
-        content.className = 's2 m-4 text-center font-sans bg-[rgb(176,252,255)] text-[rgb(34,2,108)] bg-gradient-to-br from-[rgb(134,192,255)] via-[rgb(255,255,129)] to-[rgb(255,158,205)] p-6';
-    
-        document.querySelector("nav").className = 's2 flex flex-col space-y-[3vh] max-w-fit float-left z-3';
-    
-        document.querySelectorAll("nav button").forEach(button => {
-            button.className = 's2 text-nowrap';
-        });
-    
-        document.querySelector("h1").className = 's2 m-3 text-[6vh] align-center font-bold italic text-center';
-    
-        document.querySelector("h3").className = 's2 m-3 text-[3vh] align-center font-semibold text-center';
-    
-        document.getElementById("txt").className = "s2 grid grid-cols-3 gap-5";
-    
-        document.querySelectorAll("p").forEach(paragraph => {
-            paragraph.className = 's2 gap-5 p-4 bg-white shadow-lg text-[1vw] text-justify';
-        });
-    
-        const style2 = document.createElement('style');
-        style2.setAttribute('data-dynamic', '');
-        style2.innerHTML = `
-            .s2 p:hover {
-                color: rgb(136, 29, 47);
-                background-image: linear-gradient(to bottom right, rgb(255, 213, 252), rgb(193, 255, 190), rgb(255, 243, 204));
-                box-shadow: 10px 10px 22px white;
-                border-radius: 10px 5px;
-            }
-            .s2 button {
-                background-color: rgb(255, 230, 161);
-                border-radius: 17px;
-                font-size: 22px;
-                padding: 10px;
-                width: fit-content;
-                height: fit-content;
-                color: rgb(37, 0, 42);
-            }
-            .s2 p:hover, .s2 button:hover {
-                background-color: white !important;
-                color: purple !important;
-                box-shadow: 5px 5px 15px rgb(255, 239, 174);
-                animation: glow2 3.5s ease-in-out infinite alternate !important;
-            }
-            @keyframes glow2 {
-                0% {
-                    box-shadow: 0 0 10px rgb(255, 255, 255), 
-                                0 0 20px rgb(255, 100, 61),
-                                0 0 30px rgb(255, 203, 61),
-                                0 0 40px rgb(101, 255, 74),
-                                0 0 50px rgb(137, 220, 255),
-                                0 0 60px #3878ef,
-                                0 0 70px rgb(255, 110, 216);
+            content.className = 's2 m-4 text-center font-sans bg-[rgb(176,252,255)] text-[rgb(34,2,108)] bg-gradient-to-br from-[rgb(134,192,255)] via-[rgb(255,255,129)] to-[rgb(255,158,205)] p-6';
+        
+            document.querySelector("nav").className = 's2 flex flex-col space-y-[3vh] max-w-fit float-left z-3';
+        
+            document.querySelectorAll("nav button").forEach(button => {
+                button.className = 's2 text-nowrap';
+            });
+        
+            document.querySelector("h1").className = 's2 m-3 text-[6vh] align-center font-bold italic text-center';
+        
+            document.querySelector("h3").className = 's2 m-3 text-[3vh] align-center font-semibold text-center';
+        
+            document.getElementById("txt").className = "s2 grid grid-cols-3 gap-5";
+        
+            document.querySelectorAll("p").forEach(paragraph => {
+                paragraph.className = 's2 gap-5 p-4 bg-white shadow-lg text-[1vw] text-justify';
+            });
+        
+            const style2 = document.createElement('style');
+            style2.setAttribute('data-dynamic', '');
+            style2.innerHTML = `
+                .s2 p:hover {
+                    color: rgb(136, 29, 47);
+                    background-image: linear-gradient(to bottom right, rgb(255, 213, 252), rgb(193, 255, 190), rgb(255, 243, 204));
+                    box-shadow: 10px 10px 22px white;
+                    border-radius: 10px 5px;
                 }
-                100% {
-                    box-shadow: 0 0 20px rgb(255, 255, 255),
-                                0 0 30px rgb(255, 71, 61),
-                                0 0 40px rgb(255, 203, 61),
-                                0 0 50px rgb(135, 255, 61),
-                                0 0 60px rgb(118, 237, 255),
-                                0 0 70px rgb(71, 61, 255),
-                                0 0 80px rgb(255, 120, 174);
+                .s2 button {
+                    background-color: rgb(255, 230, 161);
+                    border-radius: 17px;
+                    font-size: 22px;
+                    padding: 10px;
+                    width: fit-content;
+                    height: fit-content;
+                    color: rgb(37, 0, 42);
                 }
-            }`;
-    
-        document.head.appendChild(style2);
-        break;
-    
-
-
-        case 3:
-            content.className = 's3 bg-[rgb(11,18,18)] w-full p-9 text-white text-[35px] box-border font-serif text-center';
+                .s2 p:hover, .s2 button:hover {
+                    background-color: white !important;
+                    color: purple !important;
+                    box-shadow: 5px 5px 15px rgb(255, 239, 174);
+                    animation: glow2 3.5s ease-in-out infinite alternate !important;
+                }
+                @keyframes glow2 {
+                    0% {
+                        box-shadow: 0 0 10px rgb(255, 255, 255), 
+                                    0 0 20px rgb(255, 100, 61),
+                                    0 0 30px rgb(255, 203, 61),
+                                    0 0 40px rgb(101, 255, 74),
+                                    0 0 50px rgb(137, 220, 255),
+                                    0 0 60px #3878ef,
+                                    0 0 70px rgb(255, 110, 216);
+                    }
+                    100% {
+                        box-shadow: 0 0 20px rgb(255, 255, 255),
+                                    0 0 30px rgb(255, 71, 61),
+                                    0 0 40px rgb(255, 203, 61),
+                                    0 0 50px rgb(135, 255, 61),
+                                    0 0 60px rgb(118, 237, 255),
+                                    0 0 70px rgb(71, 61, 255),
+                                    0 0 80px rgb(255, 120, 174);
+                    }
+                }`;
+        
+            document.head.appendChild(style2);
+            break;
+        
+            case 3: //black bg
+                content.className = 's3 bg-[rgb(11,18,18)] w-full p-9 text-white text-[35px] box-border font-serif text-center';
+                document.querySelectorAll("p").forEach(paragraph => {
+                    paragraph.className = ' s3 p-4 rounded shadow-lg text-[1.2vw] hover:font-bold';
+                });
+                document.querySelector("h1").className = 's3 m-3 text-[6vh] align-center font-bold text-center';
+                document.querySelector("h3").className = 's3  m-3 text-[3vh] align-center font-semibold italic text-center';
+                document.querySelector("nav").className = 's3 m-3 text-[6vh] align-center text-center';
+                document.querySelectorAll("nav button").forEach(button => {
+                    button.className = 's3 text-nowrap hover:font-bold';
+                });
             break;
 
-        case 4: 
-            content.className = 's4 bg-[rgb(255,207,225)] w-full p-11 text-[rgb(48,0,0)] text-[45px] box-border font-impact text-center';
+        case 4: //pink bg
+            content.className = 's4 w-full p-11 bg-[rgb(255,207,225)] text-[rgb(48,0,0)] text-[45px] box-border font-impact text-right';
+            document.querySelectorAll("p").forEach(paragraph => {
+                paragraph.className = 's4 p-4 rounded shadow-lg text-[1.2vw] bg-purple-300 hover:font-bold';
+            });
+            document.querySelector("h1").className = 's4 m-3 text-[6vh] align-center font-bold text-center';
+            document.querySelector("nav").className = 's4 m-3 text-[6vh] align-center text-center';
+            document.querySelector("h3").className = 's4 m-3 text-[3vh] align-center font-semibold italic text-center';
             break;
 
-        case 5: 
+        case 5: //gradient purple
             content.className = 's5 bg-black w-fit p-6 text-white text-2xl font-sans text-left grid grid-cols-[1fr_auto] gap-5 bg-gradient-to-bl from-[rgb(42,0,33)] via-[rgb(55,0,22)] via-[rgb(47,0,67)] to-[rgb(17,0,84)]';
             document.querySelectorAll("p").forEach(paragraph => {
-                paragraph.className = 'bg-gray-800 p-4 rounded shadow-lg text-[1.2vw]';
+                paragraph.className = ' s5 bg-purple-800 p-4 m-5 rounded shadow-lg text-[1.2vw] hover:bg-pink-900';
             });
             break;
     }
-}
+};
 
 
 function s0() { applyStyle(0); }
@@ -192,6 +205,7 @@ function s2() { applyStyle(2); }
 function s3() { applyStyle(3); }
 function s4() { applyStyle(4); }
 function s5() { applyStyle(5); }
+
 const colorPicker = document.getElementById('color-picker');
 const contentDiv = document.getElementById('content');
 const colorClasses = {
@@ -202,6 +216,7 @@ const colorClasses = {
     blue: 'bg-blue-300',
     cyan: 'bg-cyan-300'
 };
+
 colorPicker.addEventListener('change', (e) => {
     const selectedColor = e.target.value;
     contentDiv.classList.remove('bg-white-300', 'bg-pink-300', 'bg-amber-300', 'bg-purple-300', 'bg-blue-300', 'bg-cyan-300');
